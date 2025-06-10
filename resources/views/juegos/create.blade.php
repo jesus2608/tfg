aaaadc46159278d05a3108202e674f80c7accc2d
jesus <jmoral.jj@gmail.com>
2025-06-10 20:54:44 +0200

    Ya si que si terminado

--- resources/views/juegos/create.blade.php
+++ resources/views/juegos/create.blade.php
@@ -178,7 +178,7 @@
                 <input type="file" name="foto1"
                     class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4
                     file:rounded-full file:border-0 file:text-sm file:font-semibold
-                    file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100" />
+                    file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100" required />
             </div>
 
             <input type="hidden" name="lat" id="lat" />
